8ff48ac847a7d9ffe514e1af55c2e9cb140cfa91
Luis Arturo Jimenez <34407545+LuisArt5@users.noreply.github.com>
2025-07-30 23:40:27 -0500

    Agrego variables de entorno para supabase

--- src/lib/supabaseclient.js
+++ src/lib/supabaseclient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xkrjnizwzbthvuxfrfrn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrcmpuaXp3emJ0aHZ1eGZyZnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MzAxODYsImV4cCI6MjA2OTUwNjE4Nn0.HD8jkoC-jRpAof5KK-U55L_JVuqCCG7bn3wwTuN-jh4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
